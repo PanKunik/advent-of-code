725b3da1baa87ef25059587c90b05d740d587e39
PanKunik <patryk.kunicki@pm.me>
2023-12-01 23:31:13 +0100

    Resolving first day (first start)

---
+++ day-1/star-1/Program.cs
@@ -0,0 +1,25 @@
+﻿var sum = 0L;
+
+using (var fileReader = File.OpenText("input.txt"))
+{
+    var line = await fileReader.ReadLineAsync();
+
+    while (line != null)
+    {
+        var number = Convert.ToInt64(
+            line.First(
+                c => char.IsDigit(c)
+            ).ToString()
+            +
+            line.Last(
+                c => char.IsDigit(c)
+            ).ToString()
+        );
+
+        sum += number;
+
+        line = await fileReader.ReadLineAsync();
+    }
+}
+
+Console.WriteLine($"Sum equals: {sum}");
