29a2801678a15f7a5400b8f959d22e10f5092e84
PanKunik <patryk.kunicki@pm.me>
2023-12-02 13:35:05 +0100

    Resolving second day (second star)

---
+++ day-2/star-2/Program.cs
@@ -0,0 +1,62 @@
+﻿using var fileReader = File.OpenText("input.txt");
+
+var sum = 0;
+
+var blueCubes = 14;
+var redCubes = 12;
+var greenCubes = 13;
+
+var line = await fileReader.ReadLineAsync();
+while (line != null)
+{
+    var gameSets = line.Split(':')[1].Split(";");
+
+    var maxBlue = -1;
+    var maxGreen = -1;
+    var maxRed = -1;
+
+    foreach (var gameSet in gameSets)
+    {
+        var blue = 0;
+        var green = 0;
+        var red = 0;
+
+        var cubes = gameSet.Split(",");
+
+        foreach (var cube in cubes)
+        {
+            if (cube.ToLower().Contains("red"))
+            {
+                red += Convert.ToInt32(cube.ToLower().Replace(" ", "").Replace("red", ""));
+            }
+            else if (cube.ToLower().Contains("green"))
+            {
+                green += Convert.ToInt32(cube.ToLower().Replace(" ", "").Replace("green", ""));
+            }
+            else
+            {
+                blue += Convert.ToInt32(cube.ToLower().Replace(" ", "").Replace("blue", ""));
+            }
+        }
+
+        if (maxBlue < blue)
+        {
+            maxBlue = blue;
+        }
+
+        if (maxRed < red)
+        {
+            maxRed = red;
+        }
+
+        if (maxGreen < green)
+        {
+            maxGreen = green;
+        }
+    }
+
+    sum += maxBlue * maxGreen * maxRed;
+    line = await fileReader.ReadLineAsync();
+}
+
+Console.WriteLine($"Sum of power of games: {sum}");
